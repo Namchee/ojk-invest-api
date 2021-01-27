8708d16bb95e6d902568d45b669691bcda74cc77
Cristopher Namchee <cristophernamchee12@gmail.com>
2021-01-27 18:46:56 +0700

    feat: Temporary migration to puppeteer

---
+++ .eslintrc.js
@@ -0,0 +1,24 @@
+module.exports = {
+  'env': {
+    'es2021': true,
+    'node': true,
+  },
+  'extends': [
+    'google',
+  ],
+  'parser': '@typescript-eslint/parser',
+  'parserOptions': {
+    'ecmaVersion': 12,
+    'sourceType': 'module',
+  },
+  'plugins': [
+    '@typescript-eslint',
+  ],
+  'rules': {
+    'require-jsdoc': 'off',
+    'object-curly-spacing': [
+      'error',
+      'always',
+    ],
+  },
+};
