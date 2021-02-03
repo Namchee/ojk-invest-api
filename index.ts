3240a8cf552bc554ef19591cd5a88a13ab69aa56
Cristopher Namchee <cristophernamchee12@gmail.com>
2021-02-03 09:33:22 +0700

    fix: Create data directory from scratch if it doesn't exist

--- index.ts
+++ index.ts
@@ -1,27 +1,41 @@
 import { performance } from 'perf_hooks';
+import { existsSync, mkdirSync } from 'fs';
+import { resolve } from 'path';
 import { launch } from 'puppeteer';
 import { Scrapper } from './src/scrapper/base';
 import { IlegalScrapper } from './src/scrapper/ilegal';
 import { AppScrapper } from './src/scrapper/app';
 
 (async () => {
-  const start = performance.now();
+  const dirPath = resolve(process.cwd(), 'data');
 
-  const browser = await launch({
-    headless: true,
-    ignoreHTTPSErrors: true,
-  });
+  const dirExist = existsSync(dirPath);
 
-  const scrappers: Scrapper[] = [
-    new IlegalScrapper(browser),
-    new AppScrapper(browser),
-  ];
+  if (!dirExist) {
+    mkdirSync(dirPath);
+  }
 
-  await Promise.all(scrappers.map(scrapper => scrapper.scrapInfo()));
+  try {
+    const start = performance.now();
 
-  const end = performance.now();
+    const browser = await launch({
+      headless: true,
+      ignoreHTTPSErrors: true,
+    });
 
-  console.log(`All process was executed in ${(end - start).toFixed(3)} ms`);
+    const scrappers: Scrapper[] = [
+      new IlegalScrapper(browser),
+      new AppScrapper(browser),
+    ];
 
-  await browser.close();
+    await Promise.all(scrappers.map(scrapper => scrapper.scrapInfo()));
+
+    const end = performance.now();
+
+    console.log(`All process was executed in ${(end - start).toFixed(3)} ms`);
+
+    await browser.close();
+  } catch (err) {
+    throw err;
+  }
 })();
