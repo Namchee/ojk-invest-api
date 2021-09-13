43a74025c37a76eec1469c2e80e23039cc87996e
Cristopher Namchee <cristophernamchee12@gmail.com>
2021-09-13 20:33:37 +0700

    :sparkles: feat: Add custom error code to graphql endpoints

--- src/graphql/resolver.ts
+++ src/graphql/resolver.ts
@@ -1,4 +1,6 @@
-import { Query } from '../services/api/const';
+import { UserInputError } from 'apollo-server-errors';
+
+import { GetManyResult, GetResult, Params, Query } from '../services/api/const';
 import { validateQuery, validateParam } from './../services/api/utils';
 
 import {
@@ -13,62 +15,83 @@ import {
   getMany as getManyProducts,
   getOne as getOneProduct,
 } from '../services/api/product';
+import { ValidationError } from '../exceptions/validation';
+
+import { IllegalInvestment } from '../entity/illegal';
+import { Product } from '../entity/product';
+import { App } from '../entity/app';
+
+/**
+ * Wrapping function for resolver.
+ *
+ * @param {Function} service service method
+ * @param {Record<string, any>} args service arguments
+ * @param {Function} validator validation function
+ * @return {Promise<T>} requested data
+ */
+async function wrapResolver<T, U>(
+  service: (args: U) => Promise<T>,
+  args: Record<string, any>,
+  validator: (obj: Record<string, any>) => U,
+): Promise<T> {
+  try {
+    const obj = validator(args);
+
+    return service(obj);
+  } catch (err) {
+    const error = err as Error;
+
+    if (error instanceof ValidationError) {
+      throw new UserInputError(error.message);
+    }
+
+    throw error;
+  }
+}
 
 export const resolvers = {
   Query: {
     illegalInvestments: async (_: any, args: any) => {
-      try {
-        const query = validateQuery(args);
-
-        return getManyIllegals(query);
-      } catch (err) {
-        throw new Error(err.message);
-      }
+      return wrapResolver<GetManyResult<IllegalInvestment>, Query>(
+        getManyIllegals,
+        args,
+        validateQuery,
+      );
     },
     illegalInvestment: async (_: any, args: any) => {
-      try {
-        const param = validateParam(args);
-
-        return getOneIllegal(param);
-      } catch (err) {
-        throw new Error(err.message);
-      }
+      return wrapResolver<GetResult<IllegalInvestment>, Params>(
+        getOneIllegal,
+        args,
+        validateParam,
+      );
     },
     products: async (_: any, args: any) => {
-      try {
-        const query: Query = validateQuery(args);
-
-        return getManyProducts(query);
-      } catch (err) {
-        throw new Error(err.message);
-      }
+      return wrapResolver<GetManyResult<Product>, Query>(
+        getManyProducts,
+        args,
+        validateQuery,
+      );
     },
     product: async (_: any, args: any) => {
-      try {
-        const param = validateParam(args);
-
-        return getOneProduct(param);
-      } catch (err) {
-        throw new Error(err.message);
-      }
+      return wrapResolver<GetResult<Product>, Params>(
+        getOneProduct,
+        args,
+        validateParam,
+      );
     },
     apps: async (_: any, args: any) => {
-      try {
-        const query: Query = validateQuery(args);
-
-        return getManyApps(query);
-      } catch (err) {
-        throw new Error(err.message);
-      }
+      return wrapResolver<GetManyResult<App>, Query>(
+        getManyApps,
+        args,
+        validateQuery,
+      );
     },
     app: async (_: any, args: any) => {
-      try {
-        const param = validateParam(args);
-
-        return getOneApp(param);
-      } catch (err) {
-        throw new Error(err.message);
-      }
+      return wrapResolver<GetResult<App>, Params>(
+        getOneApp,
+        args,
+        validateParam,
+      );
     },
   },
 };
